da8cf14e6f4aebf30e9a5a9aeff2aa66c463a938
Brian Yum <yum@Brians-MacBook-Pro.local>
2019-07-09 17:47:32 -0700

    Implemented basic setup for name and occupation

--- client/src/pages/Nav/Nav.jsx
+++ client/src/pages/Nav/Nav.jsx
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { categories } from '../../constants/constants.jsx';
 import './Nav.scss';
 
 export default class Nav extends Component {
   render () {
     return (
-      <div className="nav-container">
-        {categories.map(category => {
-          const { item, icon } = category;
-          return (
-            <div className="nav-category">
-              <i class="material-icons">{icon}</i>
-              {item}
+      <Fragment>
+        <div className="nav-container">
+          <div className="name-container">
+            <div className="nav-occupation">
+              FullStack Developer
             </div>
-          )
-        })}
-      </div>
+            <div className="nav-name">
+              Brian Sai
+            </div>
+          </div>
+          <div>
+            {categories.map(category => {
+              const { item, icon } = category;
+              return (
+                <div className="nav-category">
+                  <span>
+                    <i class="material-icons">{icon}</i>
+                  </span>
+                  {/* include description css */}
+                  <span className="nav-description">
+                    {item}
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </Fragment>
     )
   }
 }
