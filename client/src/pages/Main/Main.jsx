d3b5c2f906e40b0c8502c7abb7171ae35deb826a
briansai <brianwsai@gmail.com>
2019-07-16 16:23:13 -0700

    Integrated id for each category in main.jsx for href in navigation to refer to

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -5,16 +5,46 @@ import './Main.scss';
 export default class Main extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      nameInput: '',
+      emailInput: '',
+      messageInput: '',
+    }
+  }
+
+  handleName = e => {
+    this.setState({ nameInput: e.target.value });
+  }
+
+  handleEmail = e => {
+    this.setState({ emailInput: e.target.value });
+  }
+
+  handleMessage = e => {
+    this.setState({ messageInput: e.target.value });
   }
 
   renderDescription = item => {
     if (item === 'Contact') {
       return (
-        <div>
-          Your Name: <input />
-          {' '}
-          Message: <input />
-        </div>
+        <Fragment>
+          <div className="contact-description">
+            If you'd like to connect, please reach out to me.
+          </div>
+          <div className="contact-info">
+            <div>
+              Name: <input onChnage={this.handleName}/>
+            </div>
+            <div>
+              Email: <input onChange={this.handleEmail}/>
+            </div>
+            <div>
+              Message: <textarea onChange={this.handleMessage}/>
+            </div>
+            <button>Send</button>
+          </div>
+        </Fragment>
       )
     }
   }
@@ -28,7 +58,7 @@ export default class Main extends Component {
               const { item } = category;
               return (
                 <Fragment>
-                  <div className="category-header">
+                  <div className="category-header" id={item}>
                     {item}
                   </div>
                   <div className="category-card-container">
