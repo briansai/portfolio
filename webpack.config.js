b734bfc94fcbddf8be8ccba21e36111165532ac2
briansai <brianwsai@gmail.com>
2019-10-07 12:30:32 -0700

    Reverted back to original

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,6 @@ module.exports = {
   devtool: "source-map",
   optimization: {
     minimizer: [new UglifyJsPlugin()],
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        vendors: false,
-        vendor: {
-          name: 'vendor',
-          chunks: 'all',
-          test: /node_modules/,
-        },
-      },
-    },
   },
   module: {
     rules: [
